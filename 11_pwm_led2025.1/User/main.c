d4609a867daf2cab21f299b05c150b9a7ac35ca2
zhuohualuo666666-ops <zhuohualuo666666@gmail.com>
2025-11-26 21:31:59 +0800

    11_pwm_led2025.1
    pwm驱动LED呼吸灯

---
+++ 11_pwm_led2025.1/User/main.c
@@ -0,0 +1,28 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "PWM.h"
+
+
+uint8_t i;
+
+int main(void)
+{
+	OLED_Init();
+	PWM_Init();
+	
+	
+	while(1)
+	{
+		for(i=0;i<=100;i++)
+		{
+		PWM_SETCompare1(i);
+		Delay_ms(10);
+		}
+		for(i=0;i<=100;i++)
+		{
+		PWM_SETCompare1(100-i);
+		Delay_ms(10);
+		}
+	}
+}
